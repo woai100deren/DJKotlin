82a89a74c88ab9bd5c8a04291814187f248fdd25
WANGJING <jing.wang@nx-engine.com>
2020-11-04 15:46:36 +0800

    添加说明

--- app/src/main/java/com/dj/kotlin/network/Resource.kt
+++ app/src/main/java/com/dj/kotlin/network/Resource.kt
@@ -4,6 +4,7 @@ package com.dj.kotlin.network
  * 请求结果处理类
  */
 class Resource<out T>(val status: Status, val data: T?, val message: String?) {
+    //companion object 修饰为伴生对象,伴生对象在类中只能存在一个，类似于java中的静态方法 Java 中使用类访问静态成员，静态方法。
     companion object {
         fun <T> success(data: T?) = Resource(Status.SUCCESS, data, null)
         fun <T> error(msg: String?, data: T?) = Resource(Status.ERROR, data, msg)
